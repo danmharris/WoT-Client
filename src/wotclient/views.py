c97b459dd9c73b19a0fc7bc94fee3fa8313d6886
Dan Harris <danharris1606@gmail.com>
2019-03-01 11:28:30 +0000

    Update Thing pages to reflect merge of thing description
    No longer need to make separate requests for interactions

--- src/wotclient/views.py
+++ src/wotclient/views.py
@@ -24,9 +24,9 @@ def get_thing_or_404(thing_id):
 
 def thing_single_properties(request, thing_id):
     thing = get_thing_or_404(thing_id)
-    properties_response = requests.get('http://localhost:5002/things/{}/properties'.format(thing_id)).json()
+    properties = thing.get('properties', dict())
     if request.method == 'POST':
-        filtered = {k: v for k, v in properties_response.items() if k == request.POST['property'] or request.POST['property'] == 'all'}
+        filtered = {k: v for k, v in properties.items() if k == request.POST['property'] or request.POST['property'] == 'all'}
         for k, v in filtered.items():
             try:
                 value_response = requests.get(v['forms'][0]['href'])
@@ -35,52 +35,44 @@ def thing_single_properties(request, thing_id):
             else:
                 #TODO: Parse schema to get result
                 v['value'] = value_response.text
-            properties_response[k] = v
+            properties[k] = v
     context = {
         'tab': 'properties',
         'uuid': thing_id,
         'thing': thing,
-        'properties': properties_response,
+        'properties': properties,
     }
     return render(request, 'wotclient/thing/properties.html', context)
 
 def thing_single_actions(request, thing_id):
     thing = get_thing_or_404(thing_id)
-    actions_response = requests.get('http://localhost:5002/things/{}/actions'.format(thing_id)).json()
+    actions = thing.get('actions', dict())
     context = {
         'tab': 'actions',
         'uuid': thing_id,
         'thing': thing,
-        'actions': actions_response,
+        'actions': actions,
     }
     return render(request, 'wotclient/thing/actions.html', context)
 
 def thing_single_events(request, thing_id):
     thing = get_thing_or_404(thing_id)
-    events_response = requests.get('http://localhost:5002/things/{}/events'.format(thing_id)).json()
+    events = thing.get('events', dict())
     context = {
         'tab': 'events',
         'uuid': thing_id,
         'thing': thing,
-        'events': events_response,
+        'events': events,
     }
     return render(request, 'wotclient/thing/events.html', context)
 
 def thing_single_schema(request, thing_id):
     thing = get_thing_or_404(thing_id)
 
-    # TODO: Add /all interactions to thing directory to reduce request count
-    properties = requests.get('http://localhost:5002/things/{}/properties'.format(thing_id)).json()
-    actions = requests.get('http://localhost:5002/things/{}/actions'.format(thing_id)).json()
-    events = requests.get('http://localhost:5002/things/{}/events'.format(thing_id)).json()
-
     context = {
         'tab': 'schema',
         'uuid': thing_id,
         'thing': thing,
         'thing_pretty': json.dumps(thing, indent=4),
-        'properties_pretty': json.dumps(properties, indent=4),
-        'actions_pretty': json.dumps(actions, indent=4),
-        'events_pretty': json.dumps(events, indent=4),
     }
     return render(request, 'wotclient/thing/schema.html', context)
